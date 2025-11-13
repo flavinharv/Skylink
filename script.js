7a4fd4028c0abac66b91fdb2f857b8c24e745022
Flávia <141580331+flavinharv@users.noreply.github.com>
2025-11-12 23:34:52 -0300

    Atualizando

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('btn-tema');
+    toggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      const icon = toggle.querySelector('i');
+      icon.classList.toggle('bx-moon');
+      icon.classList.toggle('bx-sun');
+    });
