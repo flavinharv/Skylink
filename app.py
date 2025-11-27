7faa7dbaf29175a1a4600e7e5d9406990a85a2f1
Flávia <rhavena926@gmail.com>
2025-11-26 22:39:02 -0300

    Organização semântica das pastas
    Referenciar outras páginas corretamente - Pendente

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/senha')
+def senha():
+    return render_template('senha.html')
+
+@app.route('/suporte')
+def suporte():
+    return render_template('suporte.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
